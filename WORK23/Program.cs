70d39a7be933e414a8b48dd4f00d6f168e648e3e
gusev76 <gusev76dn@mail.ru>
2022-10-09 10:17:38 +0300

    добавили папкуWORK23

---
+++ WORK23/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 23
+
+int Number(string message)
+{
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    int value = Convert.ToInt32(input);
+    return value;
+}
+int number = Number ("Введите число: ");
+
+for (int i = 1; i <= number; i++)
+{
+    double result = Math.Pow(i, 3);
+    Console.WriteLine(result + ",");
+    Console.Write(result);
+}
